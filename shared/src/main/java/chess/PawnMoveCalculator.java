791e0e0a2fbb4420143c70bfc85d0be44321ae14
Eliza Freestone <elizarfreestone@gmail.com>
2025-01-16 16:08:51 -0700

    pass pawn move tests

--- shared/src/main/java/chess/PawnMoveCalculator.java
+++ shared/src/main/java/chess/PawnMoveCalculator.java
@@ -11,38 +11,57 @@ public class PawnMoveCalculator extends PieceMoveCalculator{
     public Collection<ChessMove> getPossibleMoves() {
         Collection<ChessMove> possibleMoves = super.getPossibleMoves();
         int forwardOne = 1, firstRow = 2;
-        ChessPiece.PieceType promote = null;
         if(pieceColor == ChessGame.TeamColor.BLACK){forwardOne = -1; firstRow = 7;}
         ChessPosition endPosition = new ChessPosition(myPosition.getRow(), myPosition.getColumn());
         if(myPosition.getRow() + forwardOne <= 8 && myPosition.getRow() + forwardOne >= 1) {
-            if (myPosition.getRow() + forwardOne == 8 || myPosition.getRow() + forwardOne == 1) {promote = ChessPiece.PieceType.QUEEN;}
-            // actually, add moves with promotion piece as QUEEN, BISHOP, KNIGHT, and ROOK
-
             // forward one
             endPosition = new ChessPosition(endPosition.getRow() + forwardOne, endPosition.getColumn());
             if (board.getPiece(endPosition) == null) {
-                possibleMoves.add(new ChessMove(myPosition, endPosition, promote));
+                if (myPosition.getRow() + forwardOne == 8 || myPosition.getRow() + forwardOne == 1) {
+                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.QUEEN));
+                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.BISHOP));
+                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.KNIGHT));
+                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.ROOK));
+                } else possibleMoves.add(new ChessMove(myPosition, endPosition, null));
             }
-            // capture left
-            endPosition = new ChessPosition(myPosition.getRow() + forwardOne, myPosition.getColumn() - 1);
-            if(board.getPiece(endPosition) != null && endPosition.getColumn() >= 1){
-                if(board.getPiece(endPosition).getTeamColor() != pieceColor) {
-                    possibleMoves.add(new ChessMove(myPosition, endPosition, promote));
+            // capture left (update logic so you don't go out of bounds. check bounds first, then set new endPosition, then check for enemies)
+
+            if(myPosition.getColumn() > 1){
+                endPosition = new ChessPosition(myPosition.getRow() + forwardOne, myPosition.getColumn() - 1);
+                if(board.getPiece(endPosition) != null && board.getPiece(endPosition).getTeamColor() != pieceColor) {
+                    if (myPosition.getRow() + forwardOne == 8 || myPosition.getRow() + forwardOne == 1) {
+                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.QUEEN));
+                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.BISHOP));
+                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.KNIGHT));
+                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.ROOK));
+                    } else possibleMoves.add(new ChessMove(myPosition, endPosition, null));
                 }
             }
             // capture right
-            endPosition = new ChessPosition(myPosition.getRow() + forwardOne, myPosition.getColumn() + 1);
-            if(board.getPiece(endPosition) != null && endPosition.getColumn() <= 8){
-                if(board.getPiece(endPosition).getTeamColor() != pieceColor) {
-                    possibleMoves.add(new ChessMove(myPosition, endPosition, promote));
+            if(myPosition.getColumn() < 8){
+                endPosition = new ChessPosition(myPosition.getRow() + forwardOne, myPosition.getColumn() + 1);
+                if(board.getPiece(endPosition) != null && board.getPiece(endPosition).getTeamColor() != pieceColor) {
+                    if (myPosition.getRow() + forwardOne == 8 || myPosition.getRow() + forwardOne == 1) {
+                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.QUEEN));
+                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.BISHOP));
+                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.KNIGHT));
+                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.ROOK));
+                    } else possibleMoves.add(new ChessMove(myPosition, endPosition, null));
                 }
             }
         }
         // forward two
         if(myPosition.getRow() == firstRow){
             endPosition = new ChessPosition(myPosition.getRow() + 2 * forwardOne, myPosition.getColumn());
-            if (board.getPiece(endPosition) == null) {
-                possibleMoves.add(new ChessMove(myPosition, endPosition, promote));
+            ChessPosition penguin = new ChessPosition(myPosition.getRow() + forwardOne, myPosition.getColumn());
+            if (board.getPiece(endPosition) == null && board.getPiece(penguin) == null) {
+                endPosition = new ChessPosition(myPosition.getRow() + 2 * forwardOne, myPosition.getColumn());
+                if (myPosition.getRow() + forwardOne == 8 || myPosition.getRow() + forwardOne == 1) {
+                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.QUEEN));
+                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.BISHOP));
+                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.KNIGHT));
+                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.ROOK));
+                } else possibleMoves.add(new ChessMove(myPosition, endPosition, null));
             }
         }
         return possibleMoves;
