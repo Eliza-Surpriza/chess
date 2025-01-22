461c4e08447fed7aa29611b1b6fee77cc70ddda0
Eliza Freestone <elizarfreestone@gmail.com>
2025-01-22 10:39:02 -0700

    add helper function to simplify pawn move calculator

--- shared/src/main/java/chess/PawnMoveCalculator.java
+++ shared/src/main/java/chess/PawnMoveCalculator.java
@@ -5,11 +5,19 @@ import java.util.Collection;
 public class PawnMoveCalculator extends PieceMoveCalculator{
     public PawnMoveCalculator(ChessPiece.PieceType type, ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor pieceColor) {
         super(type, board, myPosition, pieceColor);
+    }
 
+    public void addPosition(ChessPosition end, int forwardOne) {
+        if (myPosition.getRow() + forwardOne == 8 || myPosition.getRow() + forwardOne == 1) {
+            possibleMoves.add(new ChessMove(myPosition, end, ChessPiece.PieceType.QUEEN));
+            possibleMoves.add(new ChessMove(myPosition, end, ChessPiece.PieceType.BISHOP));
+            possibleMoves.add(new ChessMove(myPosition, end, ChessPiece.PieceType.KNIGHT));
+            possibleMoves.add(new ChessMove(myPosition, end, ChessPiece.PieceType.ROOK));
+        } else possibleMoves.add(new ChessMove(myPosition, end, null));
     }
 
     public Collection<ChessMove> getPossibleMoves() {
-        Collection<ChessMove> possibleMoves = super.getPossibleMoves();
+
         int forwardOne = 1, firstRow = 2;
         if(pieceColor == ChessGame.TeamColor.BLACK){forwardOne = -1; firstRow = 7;}
         ChessPosition endPosition = new ChessPosition(myPosition.getRow(), myPosition.getColumn());
@@ -17,36 +25,21 @@ public class PawnMoveCalculator extends PieceMoveCalculator{
             // forward one
             endPosition = new ChessPosition(endPosition.getRow() + forwardOne, endPosition.getColumn());
             if (board.getPiece(endPosition) == null) {
-                if (myPosition.getRow() + forwardOne == 8 || myPosition.getRow() + forwardOne == 1) {
-                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.QUEEN));
-                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.BISHOP));
-                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.KNIGHT));
-                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.ROOK));
-                } else possibleMoves.add(new ChessMove(myPosition, endPosition, null));
+                addPosition(endPosition, forwardOne);
             }
             // capture left (update logic so you don't go out of bounds. check bounds first, then set new endPosition, then check for enemies)
 
             if(myPosition.getColumn() > 1){
                 endPosition = new ChessPosition(myPosition.getRow() + forwardOne, myPosition.getColumn() - 1);
                 if(board.getPiece(endPosition) != null && board.getPiece(endPosition).getTeamColor() != pieceColor) {
-                    if (myPosition.getRow() + forwardOne == 8 || myPosition.getRow() + forwardOne == 1) {
-                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.QUEEN));
-                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.BISHOP));
-                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.KNIGHT));
-                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.ROOK));
-                    } else possibleMoves.add(new ChessMove(myPosition, endPosition, null));
+                    addPosition(endPosition, forwardOne);
                 }
             }
             // capture right
             if(myPosition.getColumn() < 8){
                 endPosition = new ChessPosition(myPosition.getRow() + forwardOne, myPosition.getColumn() + 1);
                 if(board.getPiece(endPosition) != null && board.getPiece(endPosition).getTeamColor() != pieceColor) {
-                    if (myPosition.getRow() + forwardOne == 8 || myPosition.getRow() + forwardOne == 1) {
-                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.QUEEN));
-                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.BISHOP));
-                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.KNIGHT));
-                        possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.ROOK));
-                    } else possibleMoves.add(new ChessMove(myPosition, endPosition, null));
+                    addPosition(endPosition, forwardOne);
                 }
             }
         }
@@ -56,12 +49,7 @@ public class PawnMoveCalculator extends PieceMoveCalculator{
             ChessPosition penguin = new ChessPosition(myPosition.getRow() + forwardOne, myPosition.getColumn());
             if (board.getPiece(endPosition) == null && board.getPiece(penguin) == null) {
                 endPosition = new ChessPosition(myPosition.getRow() + 2 * forwardOne, myPosition.getColumn());
-                if (myPosition.getRow() + forwardOne == 8 || myPosition.getRow() + forwardOne == 1) {
-                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.QUEEN));
-                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.BISHOP));
-                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.KNIGHT));
-                    possibleMoves.add(new ChessMove(myPosition, endPosition, ChessPiece.PieceType.ROOK));
-                } else possibleMoves.add(new ChessMove(myPosition, endPosition, null));
+                addPosition(endPosition, forwardOne);
             }
         }
         return possibleMoves;
